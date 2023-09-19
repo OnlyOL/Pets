9bfd6a9e39714f8744522ebf415497ad7ab4024c
OnlyOL <callofcthulhu123a@gmail.com>
2023-09-19 17:00:22 +0300

    Creating CRUD for cats

--- cats/urls.py
+++ cats/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 from rest_framework import routers
-from .views import UserViewSet, GroupViewSet, CatViewSet
+from .views import *
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -9,5 +9,9 @@ router.register(r'cats', CatViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('create/', add_cat, name='add-items'),
+    path('all/', view_cats, name='view_items'),
+    path('update/<int:pk>/', update_cat, name='update-items'),
+    path('item/<int:pk>/delete/', delete_cat, name='delete-items'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- cats/views.py
+++ cats/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User, Group
+from rest_framework.generics import get_object_or_404
+
 from .models import Cat
 from rest_framework import viewsets
 from rest_framework import permissions
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import serializers
+from rest_framework import status
 
 from .serializers import UserSerializer, GroupSerializer, CatSerializer
 
@@ -25,3 +29,53 @@ class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+@api_view(['POST'])
+def add_cat(request):
+    cat = CatSerializer(data=request.data)
+
+    # validating for already existing data
+    if Cat.objects.filter(**request.data).exists():
+        raise serializers.ValidationError('This data already exists')
+
+    if cat.is_valid():
+        cat.save()
+        return Response(cat.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['GET'])
+def view_cats(request):
+    # checking for the parameters from the URL
+    if request.query_params:
+        cats = Cat.objects.filter(**request.query_params.dict())
+    else:
+        cats = Cat.objects.all()
+
+    # if there is something in items else raise error
+    if cats:
+        serializer = CatSerializer(cats, many=True)
+        return Response(serializer.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['POST'])
+def update_cat(request, pk):
+    cat = Cat.objects.get(pk=pk)
+    data = CatSerializer(instance=cat, data=request.data)
+
+    if data.is_valid():
+        data.save()
+        return Response(data.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['DELETE'])
+def delete_cat(request, pk):
+    cat = get_object_or_404(Cat, pk=pk)
+    cat.delete()
+    return Response(status=status.HTTP_202_ACCEPTED)
