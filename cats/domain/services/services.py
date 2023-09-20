957b952c82b150adb8da5d3d16c929ec29c07cbe
OnlyOL <callofcthulhu123a@gmail.com>
2023-09-20 15:45:22 +0300

    services.py

---
+++ cats/domain/services/services.py
@@ -0,0 +1,27 @@
+from typing import Optional
+from cats.domain.daos import CatsDAO
+
+
+class CatsService:
+    def __int__(self):
+        self.cat_dao = CatsDAO()
+
+    def create_cat(
+            self,
+            cat_id: int,
+            name: str,
+            breed: str,
+            age: int,
+            color: str,
+            is_neutered: bool,
+            owner: int,
+    ):
+        return self.cat_dao.create_cat(
+            cat_id,
+            name,
+            breed,
+            age,
+            color,
+            is_neutered,
+            owner,
+        )
